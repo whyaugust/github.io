b1fc43eedc9dc8c69f25240c2cf1bf13c09fa88c
whyaugust <145462983+whyaugust@users.noreply.github.com>
2023-11-15 10:12:44 +0300

    Delete domashki directory

--- domashki/js/chernovik.js
+++
@@ -1,7 +0,0 @@
-const changeText = () => {
-  // Выбираем элемент на странице, и меняем содержимое нужного поля
-  document.getElementsByClassName('block4')[0].textContent = "Chumakova";
-  document.getElementsByClassName('block5')[0].textContent = "Elizaveta";
-  document.getElementsByClassName('block6')[0].textContent = "Alexandrovna";
-  document.getElementsByClassName('block8')[0].textContent = "I.I.MMIII";
-}
