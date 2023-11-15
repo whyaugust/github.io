0609aaa39a7b520e86162d98d690a5e2a9e192c1
whyaugust <145462983+whyaugust@users.noreply.github.com>
2023-11-15 10:24:49 +0300

    Add files via upload

---
+++ domashki/js/main.js
@@ -0,0 +1,26 @@
+const a = document.getElementById('in_a');
+const b = document.getElementById('in_b');
+const c = document.getElementById('in_c');
+function ab(){
+    const x= +a.value,
+          y= +b.value,
+          z= +c.value,
+          treug = ((x + y > z) && (y + z > x)&& (x + z > y)),
+          pryam = ((x**2+y**2===z**2)||(x**2+z**2===y**2)||(z**2+y**2===x**2));
+
+
+
+
+
+    if (treug && pryam){
+        alert('такой треугольник существует и он прямоугольный');
+    } else if (treug && !pryam){
+        alert('такой треугольник существует, но он не прямоугольный')
+    } else {
+        alert('это не треугольник')
+    }
+}
+
+
+
+
